fe0ba49e5ba0ed9e92aaa6fe305ae46c6f6421b4
Aaron-Fuller <aaron.fuller@protonmail.com>
2019-07-25 22:51:36 -0400

    Cleaned up code

--- hubs/hubs-router.js
+++ hubs/hubs-router.js
@@ -80,4 +80,7 @@ router.get('/', async (req, res) => {
     }
   });
 
+// add an endpoint that returns all the messages for a hub
+// add an endpoint for adding new message to a hub
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const server = express();
-
 const hubsRouter= require('./hubs/hubs-router.js');
 
+//Global configuration
 server.use(express.json());
 server.use('/api/hubs', hubsRouter);
+//server.user('/api/messages', messageRputer);
+//server.user('/api/users', userRouter);
 
 server.get('/', (req, res) => {
   res.send(`
